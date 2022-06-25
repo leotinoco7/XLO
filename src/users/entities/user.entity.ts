183238640fd8c0f960d8f9fb7fc96e1174f08297
leotinoco7 <leofptinoco7@gmail.com>
2022-06-24 22:22:35 -0300

    a

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -3,13 +3,7 @@ export class User {
   name: string;
   email: string;
   password: string;
-<<<<<<< HEAD
-  cpf: string;
-=======
   CPF: string;
-  imageUrl: string;
-  ranking: number;
->>>>>>> origin/development
   isAdmin?: boolean;
   createdAt?: Date;
   updatedAt?: Date;
